85f836c85169442f32af9533ec9135dd05ee4e52
dana061113 <danakobylandykyzy@gmail.com>
2026-03-08 21:07:48 +0500

    Implement LightOffCommand to turn off the light

---
+++ src/command/LightOffCommand.java
@@ -0,0 +1,18 @@
+package command;
+
+public class LightOffCommand implements ICommand {
+
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.off();
+    }
+
+    public void undo() {
+        light.on();
+    }
+}
