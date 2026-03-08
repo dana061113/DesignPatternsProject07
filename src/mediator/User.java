8ee159029122d0071fdc8a018b9c5974c07b76c9
dana061113 <danakobylandykyzy@gmail.com>
2026-03-08 21:16:14 +0500

    Create User.java

---
+++ src/mediator/User.java
@@ -0,0 +1,16 @@
+package mediator;
+
+public abstract class User {
+
+    protected IMediator mediator;
+    protected String name;
+
+    public User(IMediator mediator, String name){
+        this.mediator = mediator;
+        this.name = name;
+    }
+
+    public abstract void send(String message);
+
+    public abstract void receive(String message);
+}
