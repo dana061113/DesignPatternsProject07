26619131263f376f2fce81103d8b13f61127c5cb
dana061113 <danakobylandykyzy@gmail.com>
2026-03-08 21:06:17 +0500

    Add Door class with open and close methods

---
+++ src/command/Door.java
@@ -0,0 +1,12 @@
+package command;
+
+public class Door {
+
+    public void open() {
+        System.out.println("Door is OPEN");
+    }
+
+    public void close() {
+        System.out.println("Door is CLOSED");
+    }
+}
